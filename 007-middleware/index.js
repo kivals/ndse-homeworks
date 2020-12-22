b4a309a5cee03f4a2e28b42fe71698bfcf292386
melnik <Boherave21>
2020-12-22 22:36:29 +0300

    Add 007. Fix index.js

--- 007-middleware/index.js
+++ 007-middleware/index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-//const formData = require('express-form-data');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const { Book } = require('./models');
-const Store = require('./store');
 const booksRouter = require('./routes/book');
 const userRouter = require('./routes/user');
 
-//const store = new Store();
 const app = express();
 
 app.use(bodyParser());
@@ -16,13 +12,6 @@ app.use(cors());
 
 app.use('/api/user', userRouter);
 app.use('/api/books', booksRouter);
-
-// app.post('/api/user/login', (req, res) => {
-//   res.statusCode = 201;
-//   res.json({ id: 1, mail: 'test@mail.ru' });
-// });
-//
-//
 
 const PORT = process.env.PORT || 4000;
 
