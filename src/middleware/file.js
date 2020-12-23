73e309bfb0d8e8de1f3d10df50c054f382930a74
melnik <kivals.90@yandex.ru>
2020-12-23 16:00:32 +0300

    Add create view

--- src/middleware/file.js
+++ src/middleware/file.js
@@ -2,25 +2,30 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    console.log('file.mimeType0');
-    cb(null, 'public/books')
+    cb(null, 'public/books');
   },
   filename(req, file, cb) {
-    console.log('file.mimeType2');
-    cb(null, `${new Date().toISOString().replace(/:/g, '-')}-${file.originalname}`)
-  }
+    cb(null, `${new Date().toISOString().replace(/:/g, '-')}-${file.originalname}`);
+  },
 });
 
-const allowedTypes = ['text/xml', 'application/pdf', 'text/plain', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'];
+const allowedTypes = [
+  'text/xml',
+  'application/pdf',
+  'text/plain',
+  'application/msword',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+];
 
 const fileFilter = (req, file, cb) => {
   if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true)
+    cb(null, true);
   } else {
-    cb(null, false)
+    cb(null, false);
   }
 };
 
 module.exports = multer({
-  storage, fileFilter
+  storage,
+  fileFilter,
 });
--- src/routes/book.js
+++ src/routes/book.js
@@ -2,16 +2,42 @@ const express = require('express');
 
 const router = express.Router();
 const Store = require('../store');
+const { getFileFromReq } = require('../helper');
+const fileMiddleware = require('../middleware/file');
+const { Book } = require('../models');
 
 const store = new Store();
+
+router.get('/create', (req, res) => {
+  res.render('books/create', {
+    title: 'Создание книги',
+  });
+});
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   const book = store.getBook(id);
   res.render('books/index', {
-    title: 'Книги',
+    title: 'Просмотр книги',
     book,
   });
+});
+
+router.post('/', fileMiddleware.single('fileBook'), (req, res) => {
+  const fileBook = getFileFromReq(req);
+  const { title, description, authors, favorite, fileCover, fileName } = req.body;
+  const newBook = new Book(
+    undefined,
+    title,
+    description,
+    authors,
+    favorite,
+    fileCover,
+    fileName,
+    fileBook
+  );
+  store.setBook(newBook);
+  res.redirect('/');
 });
 
 module.exports = router;
