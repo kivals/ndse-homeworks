b4b6157a211873545944a2d11dda39c17199b3d1
melnik <kivals.90@yandex.ru>
2020-12-23 13:57:58 +0300

    Create view about the book

--- src/routes/book.js
+++ src/routes/book.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 const Store = require('../store');
-const { Book } = require('../models');
 
 const store = new Store();
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const book = store.getBook(id);
+  res.render('books/index', {
+    title: 'Книги',
+    book,
+  });
 });
 
 module.exports = router;
